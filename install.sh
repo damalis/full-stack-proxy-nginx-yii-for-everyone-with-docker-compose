12cd1e6d166d6d13ce6f0bcc67edd4e806218f85
Erdal ALTIN <erdalaltin@gmail.com>
2022-03-29 10:07:51 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ clear
 echo ""
 echo "================================================================="
 echo "|  								|"
-echo "|  full-stack-yiiframework-for-everyone-with-docker-compose        |"
+echo "|  full-stack-yiiframework-for-everyone-with-docker-compose     |"
 echo "|	 		  by Erdal ALTIN			|"
 echo "|  								|"
 echo "================================================================="
