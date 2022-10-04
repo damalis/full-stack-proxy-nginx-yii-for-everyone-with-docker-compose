bea06d159bd250d377f0d491b50099bf3fe35ef2
Erdal ALTIN <erdalaltin@gmail.com>
2022-10-04 12:33:00 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,12 +2,12 @@
 
 clear
 echo ""
-echo "============================================================================"
-echo "|                                                                          |"
-echo "|   full-stack-proxy-nginx-yiiframework-for-everyone-with-docker-compose   |"
-echo "|                           by Erdal ALTIN                                 |"
-echo "|                                                                          |"
-echo "============================================================================"
+echo "====================================================================="
+echo "|                                                                   |"
+echo "|   full-stack-proxy-nginx-yii-for-everyone-with-docker-compose     |"
+echo "|                           by Erdal ALTIN                          |"
+echo "|                                                                   |"
+echo "====================================================================="
 sleep 2
 
 # Uninstall old versions
